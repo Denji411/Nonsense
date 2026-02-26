54ee448cc79130c22e737210be93e212d4a1e10c
Denji411 <daniele.matranga411@gmail.com>
2026-02-26 23:57:46 +0100

    init

---
+++ StringGenerator.java
@@ -0,0 +1,15 @@
+
+import java.nio.file.Path;
+
+public class StringGenerator {
+    public static void main(String[] args) {
+        Path incipit = Path.of("resources", "incipit.txt");
+        Path frasi = Path.of("resources", "frasi.txt");
+        Path colpiDiScena = Path.of("resources", "colpi_di_scena.txt");
+        Path finali = Path.of("resources", "finali.txt");
+        Path congiunzioni = Path.of("resources", "congiunzioni.txt");
+        Path romanzo = Path.of("resources", "romanzo.txt");
+
+        
+    }
+}
