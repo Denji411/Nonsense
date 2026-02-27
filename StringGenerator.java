b85b426a2e982623fe3a2da578fbaf384317c10b
Denji411 <daniele.matranga411@gmail.com>
2026-02-27 18:23:09 +0100

    lettura file aggiunta

--- StringGenerator.java
+++ StringGenerator.java
@@ -1,15 +1,32 @@
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.*;
 
 public class StringGenerator {
     public static void main(String[] args) {
-        Path incipit = Path.of("resources", "incipit.txt");
-        Path frasi = Path.of("resources", "frasi.txt");
-        Path colpiDiScena = Path.of("resources", "colpi_di_scena.txt");
-        Path finali = Path.of("resources", "finali.txt");
-        Path congiunzioni = Path.of("resources", "congiunzioni.txt");
-        Path romanzo = Path.of("resources", "romanzo.txt");
+        Path pathIncipit = Path.of("resources", "incipit.txt");
+        Path pathFrasi = Path.of("resources", "frasi.txt");
+        Path pathColpiDiScena = Path.of("resources", "colpi_di_scena.txt");
+        Path pathFinali = Path.of("resources", "finali.txt");
+        Path pathCongiunzioni = Path.of("resources", "congiunzioni.txt");
+        Path pathRomanzo = Path.of("resources", "romanzo.txt");
 
-        
+        List<String> stringIncipit = new ArrayList<>();
+        List<String> stringFrasi = new ArrayList<>();
+        List<String> stringColpiDiScena = new ArrayList<>();
+        List<String> stringFinali = new ArrayList<>();
+        List<String> stringCongiunzioni = new ArrayList<>();
+
+        try {
+            stringIncipit = Files.readAllLines(pathIncipit);
+            stringFrasi = Files.readAllLines(pathFrasi);
+            stringColpiDiScena = Files.readAllLines(pathColpiDiScena);
+            stringFinali = Files.readAllLines(pathFinali);
+            stringCongiunzioni = Files.readAllLines(pathCongiunzioni);
+        } catch (IOException e) {
+            System.err.println("Problema nella lettura dei file: " + e.getMessage());
+        }
     }
 }
